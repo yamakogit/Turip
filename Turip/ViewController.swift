5bc8bf8b6632fe58f72cd55fafd49e6a8b782330
Koki Yamada <koki.y2764@gmail.com>
2023-08-06 07:16:12 +0900

    Initial Commit

---
+++ Turip/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Turip
+//
+//  Created by 山田航輝 on 2023/08/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
